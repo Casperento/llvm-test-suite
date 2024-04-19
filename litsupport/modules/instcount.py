2c538ccd8c513e0850d7fa5192e162143ac2ea77
Casperento <44746868+Casperento@users.noreply.github.com>
2024-04-19 12:10:50 -0300

    Patch to run an arbitrary pass over linked .bc files
    .bc and .instcount files and metrics generation
    
    Full patch to run an arbitrary pass over linked .bc files
    
    instcount.py bugfix
    
    Full patch to apply an arbitrary pass over .bc files
    
    new patch to run passes over bc files
    
    .size generation command
    
    fix the order of the custom commands
    
    missing flags fixed
    
    passes args var.
    
    fix ordering of bc and binary generation
    
    O0 cmake config

---
+++ litsupport/modules/instcount.py
@@ -0,0 +1,29 @@
+"""Test module to collect code size metrics of the benchmark executable."""
+import os.path
+
+
+def sumInstCount(filepath, metrics):
+    with open(filepath, 'r') as fp:
+        for line in fp:
+            line = line.strip()
+            if line == '':
+                continue
+            instcount = line.split(' instcount')
+            metrics['instcount'] += int(instcount[0])
+
+
+def _getInstCount(context):
+    metrics = {}
+    metrics['instcount'] = 0
+    testname = context.test.getSourcePath()
+    testdirname = os.path.dirname(testname)
+    if os.path.exists(testdirname):
+        for ff in os.listdir(testdirname):
+            if ff.endswith('.instcount'):
+                filepath = os.path.join(testdirname, ff)
+                sumInstCount(filepath, metrics)
+    return metrics
+
+
+def mutatePlan(context, plan):
+    plan.metric_collectors.append(_getInstCount)
